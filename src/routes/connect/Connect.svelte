829552e835e0eb0910198393e802db00e6f8e681
Genebson <mauryavero@gmail.com>
2022-02-01 23:03:15 -0300

    Add popup feature

--- src/routes/connect/Connect.svelte
+++ src/routes/connect/Connect.svelte
@@ -10,6 +10,8 @@
             const stellarKeyPair = await getStellarKeypair(key);
             encryptPrivateKey(key);
             $publicKey = stellarKeyPair.publicKey();
+            window.postMessage(localStorage.setItem('publicKey', $publicKey));
+            window.close();
         } catch (e) {
             if (e instanceof InvalidPrivateKeyError) {
                 $publicKey = '';
@@ -30,32 +32,79 @@
     })();
 </script>
 
-<h1>Connector</h1>
-<ul class="simple-signer wallet-container">
-    <li class="simple-signer albedo-wallet">Connect with Albedo</li>
-    <li class="simple-signer xbull-wallet">Connect with xBull</li>
-    <li class="simple-signer private-key-wallet">Connect with Private Key</li>
-</ul>
-
-{#if $publicKey}
-    <h1 id="title">Public Key: {$publicKey}</h1>
-{:else}
-    <h1 id="title">Public Key: {$connectionError ? $connectionError : 'waiting connection...'}</h1>
-{/if}
-
-<button on:click="{() => ($isPrivateKeyVisible = !$isPrivateKeyVisible)}">Show key</button>
-
-{#if $isPrivateKeyVisible}
-    <input id="input-key" type="text" bind:value="{$inputValue}" />
-{:else}
-    <input id="input-key" type="password" bind:value="{$inputValue}" />
-{/if}
-
-<button
-    class="simple-signer private-key-btn"
-    on:click="{() => {
-        connectWithSecretKey($inputValue);
-    }}"
+<div
+    class="bg-gradient-to-r
+from-red-600
+to-blue-400"
 >
-    Connect with private key
-</button>
+    <div
+        class="simple-signer connect-container w-9/12 m-auto py-16 min-h-screen flex flex-wrap items-center justify-center"
+    >
+        <button
+            on:click="{() => ($isPrivateKeyVisible = !$isPrivateKeyVisible)}"
+            type="button"
+            class="simple-signer modal-sign-btn text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2"
+            >Show Key</button
+        >
+        <div class="relative mt-1">
+            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                <svg
+                    class="h-6 w-6 text-black"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    stroke-width="2"
+                    stroke="currentColor"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                >
+                    <path stroke="none" d="M0 0h24v24H0z"></path> <circle cx="8" cy="15" r="4"></circle>
+                    <line x1="10.85" y1="12.15" x2="19" y2="4"></line> <line x1="18" y1="5" x2="20" y2="7"></line>
+                    <line x1="15" y1="8" x2="17" y2="10"></line></svg
+                >
+            </div>
+            {#if $isPrivateKeyVisible}
+                <input
+                    bind:value="{$inputValue}"
+                    type="text"
+                    class="simple-signer connect-private-key max-w-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Enter your Private Key"
+                />
+            {:else}
+                <input
+                    bind:value="{$inputValue}"
+                    type="password"
+                    class="simple-signer connect-private-key max-w-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Enter your Private Key"
+                />
+            {/if}
+            {#if $publicKey}
+                <p class="simple-signer connect-private-key-success">Public Key: {$publicKey}</p>
+            {:else}
+                <p class="simple-signer connect-private-key-error">{$connectionError ? $connectionError : ''}</p>
+            {/if}
+        </div>
+        <button
+            class="simple-signer private-key-btn text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2"
+            on:click="{() => {
+                connectWithSecretKey($inputValue);
+            }}">Connect</button
+        >
+    </div>
+</div>
+
+<!-- <button
+    id="id"
+    on:click="{() => {
+        getPublicKey();
+    }}">SEND</button
+> -->
+<style>
+    .connect-private-key-success {
+        color: #def7ec;
+    }
+    .connect-private-key-error {
+        color: red;
+    }
+</style>
