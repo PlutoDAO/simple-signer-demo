e2d0a9a4bf6803eb2f827a9706f18e491a08ce8e
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2022-02-24 12:14:25 -0300

    Add host var

--- src/routes/home/helpers/homeHelper.ts
+++ src/routes/home/helpers/homeHelper.ts
@@ -1,23 +1,17 @@
+const simpleSignerHost = process.env['VITE_HOST_SIMPLE_SIGNER'];
+
 export function openConnectWindow() {
-    const connectWindow = window.open(
-        `${process.env['VITE_HOST_SIMPLE_SIGNER']}/connect`,
-        'Connect_Window',
-        'width=280, height=350',
-    );
+    const connectWindow = window.open(`${simpleSignerHost}/connect`, 'Connect_Window', 'width=280, height=350');
     return connectWindow;
 }
 export async function openSignWindow(xdr: string, description?: string) {
-    const signWindow = window.open(
-        `${process.env['VITE_HOST_SIMPLE_SIGNER']}/sign`,
-        'Sign_Window',
-        'width=450, height=350',
-    );
+    const signWindow = window.open(`${simpleSignerHost}/sign`, 'Sign_Window', 'width=450, height=350');
 
     window.addEventListener('message', (e) => {
-        if (e.origin !== `${process.env['VITE_HOST_SIMPLE_SIGNER']}`) {
+        if (e.origin !== `${simpleSignerHost}`) {
             return;
         } else if (signWindow && e.data.ready) {
-            signWindow.postMessage({ xdr, description }, `${process.env['VITE_HOST_SIMPLE_SIGNER']}`);
+            signWindow.postMessage({ xdr, description }, `${simpleSignerHost}`);
         }
     });
 
