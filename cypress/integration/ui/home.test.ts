f75ac130423d889585274f548559393bd1fe8707
Genebson <mauryavero@gmail.com>
2022-03-03 17:44:32 -0300

    Fix port

--- cypress/integration/ui/home.test.ts
+++ cypress/integration/ui/home.test.ts
@@ -8,19 +8,19 @@ describe('home', () => {
     });
 
     it('should click sign transaction button, show popup and open window /connect', () => {
-        cy.visit('https://localhost:3002');
+        cy.visit('https://localhost:3000');
         cy.get('.sign-btn').click();
         cy.get('.modal-connect-btn').click();
     });
 
     it('should click sign transaction button, show popup and open window /sign', () => {
-        cy.visit('https://localhost:3002');
+        cy.visit('https://localhost:3000');
         cy.get('.sign-btn').click();
         cy.get('.modal-sign-btn').click();
     });
 
     it('should click sign transaction button, show popup and close it', () => {
-        cy.visit('https://localhost:3002');
+        cy.visit('https://localhost:3000');
         cy.get('.sign-btn').click();
         cy.get('.close-button').click();
     });
