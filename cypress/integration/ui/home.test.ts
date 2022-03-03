28850d180458e9a919e21079050b33fa82220db8
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2022-03-03 14:27:56 -0300

    Change url port to 3000

--- cypress/integration/ui/home.test.ts
+++ cypress/integration/ui/home.test.ts
@@ -4,19 +4,19 @@ describe('home', () => {
     // const url = Cypress.env('HOST');
 
     it('should click sign transaction button, show popup and open window /connect', () => {
-        cy.visit('https://localhost:3001');
+        cy.visit('https://localhost:3000/');
         cy.get('.sign-btn').click();
         cy.get('.modal-connect-btn').click();
     });
 
     it('should click sign transaction button, show popup and open window /sign', () => {
-        cy.visit('https://localhost:3001');
+        cy.visit('https://localhost:3000/');
         cy.get('.sign-btn').click();
         cy.get('.modal-sign-btn').click();
     });
 
     it('should click sign transaction button, show popup and close it', () => {
-        cy.visit('https://localhost:3001');
+        cy.visit('https://localhost:3000/');
         cy.get('.sign-btn').click();
         cy.get('.close-button').click();
     });
