7fd147f87b7abbec7a81b5fc689a0dbd0327a1db
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2022-03-03 15:15:28 -0300

    Change url

--- cypress/integration/ui/home.test.ts
+++ cypress/integration/ui/home.test.ts
@@ -1,26 +1,26 @@
 /// <reference types="cypress" />
 
 describe('home', () => {
-    const url = Cypress.env('HOST');
+    // const url = Cypress.env('HOST');
 
     it('should pass', () => {
         console.log('GH actions');
     });
 
     it('should click sign transaction button, show popup and open window /connect', () => {
-        cy.visit(url);
+        cy.visit('https://localhost:3002');
         cy.get('.sign-btn').click();
         cy.get('.modal-connect-btn').click();
     });
 
     it('should click sign transaction button, show popup and open window /sign', () => {
-        cy.visit(url);
+        cy.visit('https://localhost:3002');
         cy.get('.sign-btn').click();
         cy.get('.modal-sign-btn').click();
     });
 
     it('should click sign transaction button, show popup and close it', () => {
-        cy.visit(url);
+        cy.visit('https://localhost:3002');
         cy.get('.sign-btn').click();
         cy.get('.close-button').click();
     });
