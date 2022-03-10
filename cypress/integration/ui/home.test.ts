44642983a101cff353037fbc32b479d0f2f03d59
Genebson <mauryavero@gmail.com>
2022-03-10 20:18:52 -0300

    Fix URL and env var

--- cypress/integration/ui/home.test.ts
+++ cypress/integration/ui/home.test.ts
@@ -1,22 +1,22 @@
 /// <reference types="cypress" />
 
 describe('home', () => {
-    // const url = Cypress.env('HOST');
+    const url = Cypress.env('CYPRESS_BASE_URL');
 
     it('should click sign transaction button, show popup and open window /connect', () => {
-        cy.visit('https://localhost:3002/');
+        cy.visit(url);
         cy.get('.sign-btn').click();
         cy.get('.modal-connect-btn').click();
     });
 
     it('should click sign transaction button, show popup and open window /sign', () => {
-        cy.visit('https://localhost:3002/');
+        cy.visit(url);
         cy.get('.sign-btn').click();
         cy.get('.modal-sign-btn').click();
     });
 
     it('should click sign transaction button, show popup and close it', () => {
-        cy.visit('https://localhost:3002/');
+        cy.visit(url);
         cy.get('.sign-btn').click();
         cy.get('.close-button').click();
     });
