33e19e8ac06d55ca6c1fe6a0ca4e4c1182fcc147
Genebson <mauryavero@gmail.com>
2022-03-10 23:06:23 -0300

    Add new test

--- cypress/integration/ui/home.test.ts
+++ cypress/integration/ui/home.test.ts
@@ -4,13 +4,11 @@ describe('home', () => {
     it('should click sign transaction button, show popup and open window /connect', () => {
         cy.visit('/');
         cy.get('.sign-btn').click();
-        cy.get('.modal-connect-btn').click();
     });
 
     it('should click sign transaction button, show popup and open window /sign', () => {
         cy.visit('/');
         cy.get('.sign-btn').click();
-        cy.get('.modal-sign-btn').click();
     });
 
     it('should click sign transaction button, show popup and close it', () => {
