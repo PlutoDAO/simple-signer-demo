b140281f7e803db85cf96529687b9f15ca66e373
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2022-04-03 19:39:45 -0300

    Add param and change test name

--- src/routes/home/Home.svelte
+++ src/routes/home/Home.svelte
@@ -84,14 +84,16 @@
             on:click="{() => openConnectWindow()}"
             type="button"
             class="simple-signer-demo connect-btn text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2"
-            >Connect</button
         >
+            Connect
+        </button>
         <button
             on:click="{() => handleToggleSignModal()}"
             type="button"
             class="simple-signer-demo sign-btn text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2"
-            >Sign Transaction</button
         >
+            Sign Transaction
+        </button>
     </div>
     {#if $xdr != ''}
         <ToastNotification
