4649290d06e364748bed0e9010c85f9b1148a1a5
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2022-04-06 16:22:58 -0300

    Wallets filter (#7)
    * Add param and change test name
    
    * Add array

--- src/routes/home/Home.svelte
+++ src/routes/home/Home.svelte
@@ -80,7 +80,7 @@
 >
     <div class="w-9/12 m-auto py-16 min-h-screen flex items-center justify-center">
         <button
-            on:click="{() => openConnectWindow()}"
+            on:click="{() => openConnectWindow(['xbull'])}"
             type="button"
             class="simple-signer-demo connect-btn text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2"
         >
--- src/routes/home/helpers/homeHelper.ts
+++ src/routes/home/helpers/homeHelper.ts
@@ -3,11 +3,16 @@ const simpleSignerHost = process.env.VITE_HOST_SIMPLE_SIGNER;
 export function openConnectWindow(wallets?: string[]) {
     const connectWindow = window.open(`${simpleSignerHost}/connect`, 'Connect_Window', 'width=280, height=350');
 
+    let array: string[] = [];
+    if (wallets) {
+        array = wallets;
+    }
+
     window.addEventListener('message', (e) => {
         if (e.origin !== `${simpleSignerHost}`) {
             return;
         } else if (connectWindow && e.data.type === 'onReady') {
-            connectWindow.postMessage({ wallets }, `${simpleSignerHost}`);
+            connectWindow.postMessage({ wallets: array }, `${simpleSignerHost}`);
         }
     });
 
@@ -16,7 +21,7 @@ export function openConnectWindow(wallets?: string[]) {
 export async function openSignWindow(
     xdr: string,
     description?: string,
-    transactionGroups?: { from: number; to: number; description: string }[],
+    operationGroups?: { from: number; to: number; description: string }[],
 ) {
     const signWindow = window.open(`${simpleSignerHost}/sign`, 'Sign_Window', 'width=450, height=350');
 
@@ -24,7 +29,7 @@ export async function openSignWindow(
         if (e.origin !== `${simpleSignerHost}`) {
             return;
         } else if (signWindow && e.data.type === 'onReady') {
-            signWindow.postMessage({ xdr, description, transactionGroups }, `${simpleSignerHost}`);
+            signWindow.postMessage({ xdr, description, operationGroups }, `${simpleSignerHost}`);
         }
     });
 
