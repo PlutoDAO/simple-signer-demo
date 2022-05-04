252237f6401a3759e2df8c5f5a5ad1e151235d9f
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2022-05-04 19:00:30 -0300

    Refactor connect

--- src/routes/home/helpers/homeHelper.ts
+++ src/routes/home/helpers/homeHelper.ts
@@ -3,18 +3,15 @@ const simpleSignerHost = process.env.VITE_HOST_SIMPLE_SIGNER;
 export function openConnectWindow(wallets?: string[]) {
     const connectWindow = window.open(`${simpleSignerHost}/connect`, 'Connect_Window', 'width=360, height=450');
 
-    let array: string[] = [];
     if (wallets) {
-        array = wallets;
+        window.addEventListener('message', (e) => {
+            if (e.origin !== `${simpleSignerHost}`) {
+                return;
+            } else if (connectWindow && e.data.type === 'onReady') {
+                connectWindow.postMessage({ wallets }, `${simpleSignerHost}`);
+            }
+        });
     }
-
-    window.addEventListener('message', (e) => {
-        if (e.origin !== `${simpleSignerHost}`) {
-            return;
-        } else if (connectWindow && e.data.type === 'onReady') {
-            connectWindow.postMessage({ wallets: array }, `${simpleSignerHost}`);
-        }
-    });
 
     return connectWindow;
 }
